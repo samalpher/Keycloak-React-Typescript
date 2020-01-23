d08d40ef1d144c2e613ed1da98c118d1ffd1f532
Sam Alpher <salpher@carahsoft.com>
2020-01-23 09:12:54 -0500

    Remove unused import

--- src/routes/private-route.tsx
+++ src/routes/private-route.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Route, Redirect, RouteProps} from 'react-router-dom';
 
 import {useKeycloak} from "@react-keycloak/web";
